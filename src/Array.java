9066533c3c3c96e1863f2a6c6eef4e85188a3a25
KRISHNAKUMAR-M-01 <kriskanna17@gmail.com>
2026-03-04 09:33:42 +0530

    commit

--- src/Array.java
+++ src/Array.java
@@ -2,7 +2,6 @@
 import java.util.Arrays;
 public class Array {
     public static void main(String[] args) {
-        System.out.println("hello");
         String str="Hello World";
         String[] arr=str.split(" ");
         System.out.print("[");
