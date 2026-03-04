d302541be5453f6a0e608dd06fd7c6b9ec895e83
KRISHNAKUMAR-M-01 <kriskanna17@gmail.com>
2026-03-04 08:56:10 +0530

    change

--- src/NewLine.java
+++
@@ -1,9 +0,0 @@
-
-
-public class NewLine {
-    public static void main(String[] args) {
-        String str1 = "abc";
-        String str2 = "abc";
-        System.out.println(str1 +"\n"+str2);
-    }
-}
